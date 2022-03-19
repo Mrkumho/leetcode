c20676662d3589e5a232236e75db41a2a8fbe075
DESKTOP-RKHJMML\2kumh <2kumho@gmail.com>
2022-03-19 21:44:21 +0900

    896. Monotonic Array

--- 896. Monotonic Array.cpp
+++ 896. Monotonic Array.cpp
@@ -36,3 +36,44 @@ public:
         return true;
     }
 };
+
+// Runtime: 213 ms, faster than 54.69% of C++ online submissions for Monotonic Array.
+// Memory Usage: 96.3 MB, less than 99.02% of C++ online submissions for Monotonic Array.
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        return increasing(nums) or decreasing(nums);
+    }
+    
+    bool increasing(vector<int>& nums){
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i+1] < nums[i]) return false;
+        }
+        return true;
+    }
+    
+    bool decreasing(vector<int>& nums){
+        for(int i = 0; i< nums.size() -1; i++){
+            if(nums[i+1] > nums[i]) return false;
+        }
+        return true;
+    }
+};
+
+// Runtime: 195 ms, faster than 70.71% of C++ online submissions for Monotonic Array.
+// Memory Usage: 96.3 MB, less than 99.02% of C++ online submissions for Monotonic Array.
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true, decreasing = true;
+        
+        for(int i = 0; i< nums.size()-1; i++){
+            if(nums[i+1] > nums[i]) decreasing = false;
+            if(nums[i+1] < nums[i]) increasing = false;
+        }
+        
+        return increasing or decreasing;        
+    }
+};
