bc7862ba53296c86fbed2669e200ca64c28a883d
DESKTOP-RKHJMML\2kumh <2kumho@gmail.com>
2022-04-17 00:10:19 +0900

    243. Shortest Word Distance

--- Wix Engineering/Arrays and Strings.cpp
+++ Wix Engineering/Arrays and Strings.cpp
@@ -1,7 +1,6 @@
-// 929. Unique Email Addresses
-
 class Solution {
 public:
+// 929. Unique Email Addresses
     int numUniqueEmails(vector<string>& emails) {
         int cnt = 0;
         unordered_set<string> unset;
@@ -25,4 +24,20 @@ public:
         
         return cnt;
     }
+// 243. Shortest Word Distance    
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int i1 = -1;
+        int i2 = -1;
+        int ans = INT_MAX;
+        
+        for(int i = 0 ; i < wordsDict.size(); i++) {
+            if(wordsDict[i] == word1 ) i1 = i;
+            if(wordsDict[i] == word2 ) i2 = i;
+            
+            if(i1 != -1 && i2 != -1) ans = min(ans,abs(i1-i2));
+        }
+        
+        return ans;
+    }
 };
+
